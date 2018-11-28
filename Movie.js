59bea3a0498e944a32176017b32d63d4dfab064f
jiaa88 <32768927+jiaa88@users.noreply.github.com>
2018-11-28 22:30:55 +0800

    Add files via upload

---
+++ Movie.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const db = 'mongodb://jia:abc123@ds141043.mlab.com:41043/jiamovie';
+
+//debugging
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log('Connected to database');
+  })
+  .catch(error => {
+    console.log('Mongoose connetion error: ', error);
+  });
+
+const schema = mongoose.Schema({
+  title: { type: String },
+  year: { type: String },
+  genre: { type: String },
+  actors: { type: String },
+  plot: { type: String },
+  poster: { type: String }
+});
+
+const Movie = mongoose.model('Movie', schema, 'movieCollection');
+
+module.exports = Movie;
